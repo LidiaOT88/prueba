54ab21325bbbb1ff33c1d9317c9a4c47e6e21597
Lidia <lidia.ortega.toledano@hotmail.com>
2022-11-04 20:55:50 +0100

    Creation project

---
+++ app/src/main/java/com/lidorttol/pruebamercadona/NavHostActivity.kt
@@ -0,0 +1,11 @@
+package com.lidorttol.pruebamercadona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nav_host_activity)
+    }
+}
