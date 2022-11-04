0303e845e457312a5e314d414f79bd8fa6f0f3d2
Lidia <lidia.ortega.toledano@hotmail.com>
2022-11-04 22:32:25 +0100

    - Add basic elements   - Fragments   - Navigation component   - Organize structure project - Fix libraries bugs - Fix AndroidManifest - Add module

---
+++ app/src/main/java/com/lidorttol/pruebamercadona/di/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.lidorttol.pruebamercadona.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
---
+++ app/src/main/java/com/lidorttol/pruebamercadona/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.lidorttol.pruebamercadona.ui.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    application: Application
+) : AndroidViewModel(application)
